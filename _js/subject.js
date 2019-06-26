1bac2c65f7b33bc5a377633e7ff7a75c132cf823
grabowski74 <matheus.araujo@ccc.ufcg.edu.br>
2019-06-26 20:18:47 -0300

    add 'delete comments'

--- _js/subject.js
+++ _js/subject.js
@@ -37,12 +37,25 @@ function subjectProfile(id) {
                         '<div class="answer-name">' + arrayAnwser.user.primeiroNome + ' ' + arrayAnwser.user.ultimoNome + ':' + '</div>' +
                         '<div class="answer-text">' + arrayAnwser.text + ' - ' + '<span class="answer-time">' + arrayAnwser.hora + ', ' + arrayAnwser.data + '</span>' + '</div></div>');
                 }) : null;
-                listComments += ('<div class="card-comment">' +
-                    '<div class="comment-name">' + arrayItem.user.primeiroNome + ' ' + arrayItem.user.ultimoNome + ':' + '</div>' +
-                    '<div class="comment-text">' + arrayItem.text + ' - ' + '<span class="comment-time">' + arrayItem.hora + ', ' + arrayItem.data + '</span>' + '</div>' +
-                    '<div class="comment-answer">' + listAnswers + '</div>' +
-                    "<div class='answer-area'><input type='text' class='answer-submit' placeholder='digite uma resposta...' id='answer"+ arrayItem.id +"' ><a class='answer-button' href='#' onclick='return addAnswer(" + arrayItem.id + ")'><i class='fas fa-reply'></i></a></div>" +
-                    '</div>');
+                if(arrayItem.user.email == localStorage.getItem("login")){
+                    if(arrayItem.text != "" && arrayItem.text != null){
+                        listComments += ('<div class="card-comment-user">' +
+                        '<div class="comment-name">' + arrayItem.user.primeiroNome + ' ' + arrayItem.user.ultimoNome + ':' + '<a class="comment-delete" href="#" onclick="return removeComment(' + arrayItem.id + ')"><i class="fas fa-trash-alt"></i></a>' + '</div>' +
+                        '<div class="comment-text">' + arrayItem.text + ' - ' + '<span class="comment-time">' + arrayItem.hora + ', ' + arrayItem.data + '</span>' + '</div>' +
+                        '<div class="comment-answer">' + listAnswers + '</div>' +
+                        "<div class='answer-area'><input type='text' class='answer-submit' placeholder='digite uma resposta...' id='answer"+ arrayItem.id +"' ><a class='answer-button' href='#' onclick='return addAnswer(" + arrayItem.id + ")'><i class='fas fa-reply'></i></a></div>" +
+                        '</div>')
+                    }
+                } else{
+                    if(arrayItem.text != "" && arrayItem.text != null){
+                        listComments += ('<div class="card-comment">' +
+                            '<div class="comment-name">' + arrayItem.user.primeiroNome + ' ' + arrayItem.user.ultimoNome + ':' + '</div>' +
+                            '<div class="comment-text">' + arrayItem.text + ' - ' + '<span class="comment-time">' + arrayItem.hora + ', ' + arrayItem.data + '</span>' + '</div>' +
+                            '<div class="comment-answer">' + listAnswers + '</div>' +
+                            "<div class='answer-area'><input type='text' class='answer-submit' placeholder='digite uma resposta...' id='answer"+ arrayItem.id +"' ><a class='answer-button' href='#' onclick='return addAnswer(" + arrayItem.id + ")'><i class='fas fa-reply'></i></a></div>" +
+                            '</div>');
+                    }
+                }
             }) : null;
 
             document.getElementById("subjectbyid").innerHTML = profile + "</br><div><span></span><span></span><span></span></div></br>" +
@@ -157,4 +170,34 @@ function submitAnswer(inputAnswer, id){
     .catch(function(error){
         alert(error.message);
     })
+}
+
+function removeComment(id){
+    var idSubject = location.search.split("?");
+    var broke = idSubject[1].split("=");
+
+    fetch('https://api-ucdb.herokuapp.com/api/v1/perfil/comentario/delete/?Comentario-id=' + id, {
+        method: 'DELETE',
+        headers:{
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json; charset=utf-8',
+            'Authorization': `Bearer ${localStorage.token}`
+        },
+    })
+    .then(function(response){
+        var msg = ""
+        if(!response.ok){
+            msg = "Algo deu errado"
+            throw new Error("Não foi possivel apagar o comentário!")
+        }
+        return response.text()
+    })
+    .then(function(data) {
+        alert("Comentário apagado com sucesso")
+        subjectProfile(broke[1]);
+    })
+    .catch(function(error){
+        alert(error.message);
+    })
+
 }
